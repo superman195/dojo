cef1cc2d25b93208f5956ed75c15efc11656c059
jarvis8x7b <157810922+jarvis8x7b@users.noreply.github.com>
2025-02-21 19:12:23 +0800

    test(data): wip ensure sqlalchemy query fetches same data

--- test_sqlalchemy.py
+++ test_sqlalchemy.py
@@ -1,13 +1,9 @@
 import asyncio
-from datetime import datetime, timedelta, timezone
+from datetime import datetime
 
 import asyncpg
-from sqlalchemy import and_, not_, select
+from sqlalchemy import not_, select
 
-from commons.exceptions import (
-    ExpiredFromMoreThanExpireTo,
-)
-from commons.utils import datetime_as_utc
 from database.client import connect_db, disconnect_db
 from database.prisma.models import ValidatorTask
 from database.prisma.types import (
@@ -16,59 +12,57 @@ from database.prisma.types import (
 )
 from database.sqlalchemy.tables import (
     t_completion,
-    t_criterion,
-    t_ground_truth,
-    t_miner_response,
-    t_miner_score,
     t_validator_task,
 )
-from dojo import TASK_DEADLINE
 
 
-def get_validator_tasks_query(expire_from=None, expire_to=None):
-    # Set default expiry timeframe
-    if not expire_from:
-        expire_from = (
-            datetime_as_utc(datetime.now(timezone.utc))
-            - timedelta(seconds=TASK_DEADLINE)
-            - timedelta(hours=6)
-        )
-    if not expire_to:
-        expire_to = datetime_as_utc(datetime.now(timezone.utc)) - timedelta(
-            seconds=TASK_DEADLINE
-        )
+def get_validator_tasks_query():
+    # query = (
+    #     select(t_validator_task)
+    #     .join(t_completion, t_validator_task.c.id == t_completion.c.validator_task_id)
+    #     .join(t_criterion, t_completion.c.id == t_criterion.c.completion_id)
+    #     .join(t_miner_score, t_criterion.c.id == t_miner_score.c.criterion_id)
+    #     .join(
+    #         t_miner_response,
+    #         t_validator_task.c.id == t_miner_response.c.validator_task_id,
+    #     )
+    #     .join(
+    #         t_ground_truth, t_validator_task.c.id == t_ground_truth.c.validator_task_id
+    #     )
+    #     .where(
+    #         and_(
+    #             # t_validator_task.c.expire_at > expire_from,
+    #             # t_validator_task.c.expire_at < expire_to,
+    #             not_(t_validator_task.c.is_processed),
+    #         )
+    #     )
+    #     .order_by(t_validator_task.c.created_at.desc())
+    #     .limit(5)
+    # )
 
-    # Check expiry timeframe validity
-    if expire_from > expire_to:
-        raise ExpiredFromMoreThanExpireTo("expire_from should be less than expire_to.")
-
+    # query = (
+    #     select(t_validator_task)
+    #     .select_from(t_validator_task)  # Explicitly start from validator_task table
+    #     .distinct()  # Add distinct to remove duplicates
+    #     .where(
+    #         not_(t_validator_task.c.is_processed),
+    #     )
+    #     .order_by(t_validator_task.c.created_at.desc())
+    #     .limit(5)
+    # )
     query = (
         select(
+            # t_validator_task.c.id, t_validator_task.c.created_at,
             t_validator_task,
+        )  # Explicitly select columns
+        .distinct()  # Add distinct to remove duplicates
+        .join(
             t_completion,
-            t_criterion,
-            t_miner_score,
-            t_miner_response,
-            t_ground_truth,
-        )
-        .outerjoin(
-            t_completion, t_validator_task.c.id == t_completion.c.validator_task_id
-        )
-        .outerjoin(t_criterion, t_completion.c.id == t_criterion.c.completion_id)
-        .outerjoin(t_miner_score, t_criterion.c.id == t_miner_score.c.criterion_id)
-        .outerjoin(
-            t_miner_response,
-            t_validator_task.c.id == t_miner_response.c.validator_task_id,
-        )
-        .outerjoin(
-            t_ground_truth, t_validator_task.c.id == t_ground_truth.c.validator_task_id
+            t_validator_task.c.id == t_completion.c.validator_task_id,
+            isouter=True,
         )
         .where(
-            and_(
-                # t_validator_task.c.expire_at > expire_from,
-                # t_validator_task.c.expire_at < expire_to,
-                not_(t_validator_task.c.is_processed),
-            )
+            not_(t_validator_task.c.is_processed),
         )
         .order_by(t_validator_task.c.created_at.desc())
         .limit(5)
@@ -80,8 +74,6 @@ def get_validator_tasks_query(expire_from=None, expire_to=None):
     query_str = query.compile(
         dialect=postgresql.dialect(), compile_kwargs={"literal_binds": True}
     )
-
-    query = select(t_validator_task).limit(5)
 
     return query, query_str
 
@@ -97,7 +89,7 @@ async def get_validator_tasks(conn, expire_from=None, expire_to=None):
     Returns:
         List of validator task records with related data
     """
-    query = get_validator_tasks_query(expire_from, expire_to)
+    query = get_validator_tasks_query()
     result = await conn.execute(query)
     fetched_res = result.fetchall()
     print(type(fetched_res))
@@ -113,7 +105,7 @@ async def get_session():
     from sqlalchemy.ext.asyncio import create_async_engine
 
     engine = create_async_engine(
-        "postgresql://postgres:<password>@localhost/db",
+        "postgresql://postgres:mysecretpasswordlocalhost/db",
         echo=True,
     )
 
@@ -132,19 +124,26 @@ async def test_sqlalchemy():
     # Establish a connection to an existing database named "test"
     # as a "postgres" user.
     # Calculate start and end time for expiry window
-    conn = await asyncpg.connect("postgresql://postgres:<password>@localhost/db")
+    conn = await asyncpg.connect("postgresql://postgres:mysecretpassword@localhost/db")
     # Execute a statement to create a new table.
     query, query_str = get_validator_tasks_query()
     query_str = str(query_str)
-    print(query_str)
-    print(await conn.execute(query_str))
+    # print(query_str)
+    # print(await conn.execute(query_str))
 
     # Select a row from the table.
     # row = await conn.fetchrow("select * from validator_task limit 5")
-    row = await conn.fetchrow(query_str)
+    values = await conn.fetch(query_str)
+    print(type(values))
+
+    for idx, v in enumerate(values):
+        print(f"SQLALchemy got {idx}: {v}")
+
+    ids = [v["id"] for v in values]
     # *row* now contains
     # asyncpg.Record(id=1, name='Bob', dob=datetime.date(1984, 3, 1))
-    print(row)
+    # print(f"{values=}")
+    print(f"SQLAlchemy got records: {ids}")
 
     # Close the connection.
     await conn.close()
@@ -169,12 +168,14 @@ async def test_prisma():
         }
     )
 
-    await ValidatorTask.prisma().find_many(
+    res = await ValidatorTask.prisma().find_many(
         include=include_query,
         where=vali_where_query_unprocessed,
         order={"created_at": "desc"},
         take=5,
     )
+
+    print(f"Prisma query got {[r.id for r in res]}")
     await disconnect_db()
     print("Time taken for Prisma: ", datetime.now() - start_time)
 
