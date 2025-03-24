7f750855435d20e74de79494c0213c827f8667a3
codebender <167290009+codebender37@users.noreply.github.com>
2025-03-24 16:49:43 +0700

    refactor: decoupled updated_tasks and scoring

--- main_validator.py
+++ main_validator.py
@@ -71,7 +71,8 @@ async def main():
     running_tasks = [
         asyncio.create_task(validator.log_validator_status()),
         asyncio.create_task(validator.run()),
-        asyncio.create_task(validator.update_score_and_send_feedback()),
+        asyncio.create_task(validator.update_tasks_before_scoring()),
+        asyncio.create_task(validator.score_and_send_feedback()),
         asyncio.create_task(validator.send_heartbeats()),
         asyncio.create_task(
             start_block_subscriber(callbacks=[validator.block_headers_callback])
